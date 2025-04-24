7d1212650ba65b06352e7d0211b0e9d67af48878
tonynagyy <nagytony2@gmail.com>
2025-04-25 01:36:51 +0300

    feat: starter files

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // Set to false to disable dark mode
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+        accent: "#657786",
+        background: "#F5F8FA",
+        text: "#14171A",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+
+  plugins: [],
+};
