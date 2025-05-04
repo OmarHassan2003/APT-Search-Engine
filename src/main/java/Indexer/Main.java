6cf87587a71fe0fb65684bd09edbf72b87e4d1f0
tonynagyy <nagytony2@gmail.com>
2025-05-05 00:44:03 +0300

    refactor: merging client

--- src/main/java/Indexer/Main.java
+++
@@ -1,9 +0,0 @@
-package Indexer;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("[INFO] Starting the Indexer...");
-        Indexer.index();
-        System.out.println("[INFO] Indexing process completed.");
-    }
-}
